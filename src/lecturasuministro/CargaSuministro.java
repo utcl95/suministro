ba281631c18ee7c6f2aeef3d9093114e9db06bd9
Jaqueline Aquino <das5797@gmail.com>
2009-11-13 06:09:43 +0000

    nueva clase

---
+++ src/lecturasuministro/CargaSuministro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lecturasuministro;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Carlos
+ */
+public class CargaSuministro extends MIDlet {
+    public void startApp() {
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
