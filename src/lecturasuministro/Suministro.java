e10fdd98c060aecd57196322cbd32d4c6fc99c06
Jaqueline Aquino <das5797@gmail.com>
2009-11-13 07:17:20 +0000

    Agrege metodo ingresarConsumo

--- src/lecturasuministro/Suministro.java
+++ src/lecturasuministro/Suministro.java
@@ -12,6 +12,7 @@ import javax.microedition.rms.RecordStoreException;
  */
 public class Suministro extends MIDlet implements CommandListener, ItemCommandListener {
     private static final Command CMD_PRESS = new Command ("Press", Command.ITEM, 1);
+    private static final Command CMD_PRESS2 = new Command ("Press", Command.ITEM, 1);
     private static final Command CMD_EXIT = new Command ("Exit", Command.EXIT, 1);
     private Display display;
     private TextField txt1;
@@ -33,7 +34,7 @@ public class Suministro extends MIDlet implements CommandListener, ItemCommandLi
             mainForm = new Form("Lectura Suministro");
             txt1 = new TextField("Buscar", "", 15, TextField.NUMERIC);
             mainForm.append(txt1);
-            StringItem item = new StringItem("Button ", "Button", Item.BUTTON);
+            StringItem item = new StringItem("", "Buscar", Item.BUTTON);
             item.setDefaultCommand(CMD_PRESS);
             item.setItemCommandListener(this);
             mainForm.append(item);
@@ -51,6 +52,10 @@ public class Suministro extends MIDlet implements CommandListener, ItemCommandLi
              if (buscarSuministro(txt1.getString())) {
                  
              }
+        }if (c == CMD_PRESS2) {
+            if (ingresarConsumo(consumo.getString())){
+           
+            }
         }
     }
 
@@ -92,19 +97,38 @@ public class Suministro extends MIDlet implements CommandListener, ItemCommandLi
      */
     public boolean buscarSuministro(String msuministro) {
              
-        if(sRMS.searchSuministro(msuministro) == 0)
-               
-           System.out.println("NO Encontrado");
-        
-        else{
-            mainForm2 = new Form("Ingresar Suministro");
-            consumo = new TextField("Consumo   ", "", 20, TextField.NUMERIC);
-            mainForm2.append(new TextField("Suministro", txt1.getString(), 20, TextField.UNEDITABLE));
-            mainForm2.append(consumo);
-            mainForm2.setCommandListener(this);
-            display.setCurrent (mainForm2);
+        if(sRMS.searchSuministro(msuministro) == 0){
+
+           String text = "No se encontro suministro";
+           Alert a = new Alert ("Action", text, null, AlertType.INFO);
+           display.setCurrent (a);
+           
+        }else{
+
+           mainForm2 = new Form("Ingresar Suministro");
+           consumo = new TextField("Consumo   ", "", 20, TextField.NUMERIC);
+           mainForm2.append(new TextField("Suministro", txt1.getString(), 20, TextField.UNEDITABLE));
+           mainForm2.append(consumo);
+           StringItem item = new StringItem("", "Ingresar", Item.BUTTON);
+           item.setDefaultCommand(CMD_PRESS2);
+           item.setItemCommandListener(this);
+           mainForm2.append(item);
+           mainForm2.setCommandListener(this);
+           display.setCurrent (mainForm2);
            //System.out.println("Encontrado");
         }
-        return false; 
+
+        return false;
     }
+
+    public boolean ingresarConsumo(String mconsumo) {
+
+            String text = "Consumo ingresado";
+            Alert b = new Alert ("Action", text, null, AlertType.INFO);
+            display.setCurrent (b);
+
+        return false;
+    }
+
+    
 }
