f43622088d481d14e7d4cb4961dc1fdbcaefacc1
Juan Aquino <utcl95@gmail.com>
2009-11-24 03:30:41 +0000

    Actualizado

--- src/lecturasuministro/EnviarLectura.java
+++ src/lecturasuministro/EnviarLectura.java
@@ -5,6 +5,11 @@
 
 package lecturasuministro;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
 import javax.microedition.midlet.*;
 
 /**
@@ -22,20 +27,27 @@ public class EnviarLectura extends MIDlet {
     }
 
     public void sendData() {
-        String m_record[] = new String[3];
-        // Variables a exportar.
-        String t_suministro = "";
-        String t_consumo    = "";
-        String t_obs        = "";
-
-        SuministroRMS sRMS = new SuministroRMS("SUMINISTROS");
-        int numRMS = sRMS.recordCount();
-        
-        //System.out.println(numRMS);
-
-        for(int i=1; i<=numRMS; i++) {
-            m_record = sRMS.getRecord(i);
-            System.out.println(m_record[2]);
+        String[] m_record = new String[3];
+        String m_linea = "";
+        try {
+            SuministroRMS sRMS = new SuministroRMS("SUMINISTROS");
+            int numRMS = sRMS.recordCount();
+            //System.out.println(numRMS);
+            FileConnection connection = (FileConnection) Connector.open("file:///SDCard//lecturas.txt", Connector.WRITE );
+            connection.create();
+            OutputStream out = connection.openOutputStream();
+            PrintStream output = new PrintStream(out);
+            
+            for (int i = 1; i <= numRMS; i++) {
+                m_record = sRMS.getRecord(i);
+                m_record[0] = m_record[0].substring(0, 8);
+                m_linea =  m_record[0] + ";" + m_record[1] + ";" +  m_record[2];
+                output.println(m_linea);
+            }
+            out.close();
+            connection.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
