784b1f0841526537cf61e3e70fbd967839f56941
Juan Aquino <utcl95@gmail.com>
2009-12-13 14:36:44 +0000

    Usan nueva clase Validacion...

--- src/lecturasuministro/EnviarLecturaSum.java
+++ src/lecturasuministro/EnviarLecturaSum.java
@@ -34,7 +34,7 @@ public class EnviarLecturaSum extends Form implements CommandListener, ItemComma
     }
 
     public void commandAction (Command c, Item item) {
-       
+        Validacion validarSuministro = new Validacion();
         if(obs.getString().equals("")){
             vobs = 0;
         
@@ -68,106 +68,16 @@ public class EnviarLecturaSum extends Form implements CommandListener, ItemComma
                       ss.mostrarMensaje("b");
                     }
                     break;
-                case 3:
-                    if( (cons_act <= 10) && (promedio > 4 * cons_act) && (promedio > 20) ){
+                default:
+                    if(!validarSuministro.esValido(vobs, lactual, lanterior, cons_act, promedio) ) {
                       ss.mostrarMensaje("c");
                     }else{
                       if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
                     }
                     break;
-                case 4:
-                    if( (cons_act > 10) && (cons_act <= 30) && ( promedio >= 2.75 * cons_act || promedio <= 0.4 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 5:
-                    if( (cons_act > 30) && (cons_act <= 50) && ( promedio >= 2.30 * cons_act || promedio <= 0.5 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 6:
-                    if( (cons_act > 50) && (cons_act <= 70) && ( promedio >= 2 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 7:
-                    if( (cons_act > 70) && (cons_act <= 100) && ( promedio >= 1.8 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 8:
-                    if( (cons_act > 100) && (cons_act <= 150) && ( promedio >= 1.7 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 9:
-                    if( (cons_act > 150) && (cons_act <= 200) && ( promedio >= 1.5 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 10:
-                    if( (cons_act > 200) && (cons_act <= 300) && ( promedio >= 1.5 * cons_act || promedio <= 0.65 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 11:
-                    if( (cons_act > 300) && (cons_act <= 400) && ( promedio >= 1.6 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 12:
-                    if( (cons_act > 400) && (cons_act <= 500) && ( promedio >= 1.5 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 13:
-                    if( (cons_act > 500) && (cons_act <= 700) && ( promedio >= 1.4 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 14:
-                    if( (cons_act > 700) && (cons_act <= 1500) && ( promedio >= 1.3 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                 case 15:
-                    if( (cons_act > 1500) && (cons_act <= 2500) && ( promedio >= 1.2 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                 case 16:
-                    if( cons_act > 2500 && ( cons_act < 1.2 * promedio || promedio <= 0.6 * cons_act ) ) {
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-            }
-        }
+            } // End Switch
+        } // End if
+        validarSuministro = null;
     }
 
      /**
--- src/lecturasuministro/LeerConsumo.java
+++ src/lecturasuministro/LeerConsumo.java
@@ -33,10 +33,13 @@ public class LeerConsumo extends Form implements CommandListener, ItemCommandLis
     }
 
     public void commandAction (Command c, Item item) {
+        Validacion validarSuministro = new Validacion();
+        
         if(obs.getString().equals(""))
             vobs = 0;
         else
             vobs = Integer.parseInt(obs.getString());
+        
         lactual = Integer.parseInt(consumo.getString());
         int lanterior = ss.lAnterior();
         int promedio = ss.lPromedio();
@@ -65,106 +68,17 @@ public class LeerConsumo extends Form implements CommandListener, ItemCommandLis
                       ss.mostrarMensaje("b");
                     }
                     break;
-                case 3:
-                    if( (cons_act <= 10) && (promedio > 4 * cons_act) && (promedio > 20) ){
+                // Todos los casos: 3-16 son semejantes.
+                default:
+                    if(!validarSuministro.esValido(vobs, lactual, lanterior, cons_act, promedio) ) {
                       ss.mostrarMensaje("c");
                     }else{
                       if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
                     }
                     break;
-                case 4:
-                    if( (cons_act > 10) && (cons_act <= 30) && ( promedio >= 2.75 * cons_act || promedio <= 0.4 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 5:
-                    if( (cons_act > 30) && (cons_act <= 50) && ( promedio >= 2.30 * cons_act || promedio <= 0.5 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 6:
-                    if( (cons_act > 50) && (cons_act <= 70) && ( promedio >= 2 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 7:
-                    if( (cons_act > 70) && (cons_act <= 100) && ( promedio >= 1.8 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 8:
-                    if( (cons_act > 100) && (cons_act <= 150) && ( promedio >= 1.7 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 9:
-                    if( (cons_act > 150) && (cons_act <= 200) && ( promedio >= 1.5 * cons_act || promedio <= 0.6 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 10:
-                    if( (cons_act > 200) && (cons_act <= 300) && ( promedio >= 1.5 * cons_act || promedio <= 0.65 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 11:
-                    if( (cons_act > 300) && (cons_act <= 400) && ( promedio >= 1.6 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 12:
-                    if( (cons_act > 400) && (cons_act <= 500) && ( promedio >= 1.5 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 13:
-                    if( (cons_act > 500) && (cons_act <= 700) && ( promedio >= 1.4 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                case 14:
-                    if( (cons_act > 700) && (cons_act <= 1500) && ( promedio >= 1.3 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                 case 15:
-                    if( (cons_act > 1500) && (cons_act <= 2500) && ( promedio >= 1.2 * cons_act || promedio <= 0.7 * cons_act ) ){
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-                 case 16:
-                    if( cons_act > 2500 && ( cons_act < 1.2 * promedio || promedio <= 0.6 * cons_act ) ) {
-                      ss.mostrarMensaje("c");
-                    }else{
-                      if (ingresarConsumo(suministro, consumo.getString(), obs.getString())){}
-                    }
-                    break;
-            }
-        }
+            }   // end case
+        }       // end if
+        validarSuministro = null;
     }
 
      /**
