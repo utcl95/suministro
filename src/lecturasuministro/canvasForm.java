5f048abdd3e8a3a1834fa2bb8f80f72c05b9ba5e
Juan Aquino <utcl95@gmail.com>
2010-01-24 05:46:22 +0000

    canvasForm, ya no utiliza funciones de textfile.

--- src/lecturasuministro/canvasForm.java
+++ src/lecturasuministro/canvasForm.java
@@ -9,6 +9,10 @@ package lecturasuministro;
  *
  * @author Juan
  */
+import java.io.IOException;
+import java.io.InputStream;
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
 import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Graphics;
 
@@ -42,11 +46,6 @@ public class canvasForm extends Canvas {
     }
 
     protected void keyPressed(int keyCode) {
-        if (keyCode > 0) {
-          
-        } else {
-          
-        }
         // Derecha +
         if(getGameAction(keyCode) == 5) doNext();
         // Izquierda -
@@ -104,14 +103,77 @@ public class canvasForm extends Canvas {
         }
 
         public void setCurrentSuministro(int cs) {
-            TextFile txt = new TextFile("file:///SDCard//suministros.txt");
-            //TextFile txt = new TextFile("file:///e:/suministros.txt");
-            String m_linea = txt.readLine(cs);
+            String m_url = "file:///SDCard//suministros.txt";
+            //String m_url = "file:///e:/suministros.txt";
+            TextFile txt = new TextFile(m_url);
+            
+            // readline inicio
+            //String m_linea = txt.readLine(cs);
+            FileConnection ptr_file = null;
+            InputStream is_rl = null;
+            String m_linea = "";
+            int num = 0; int ch; int line=0;
+
+            line = cs;
+            try {
+                ptr_file = (FileConnection) Connector.open(m_url, Connector.READ);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            try {
+                is_rl = ptr_file.openInputStream();
+                ch = is_rl.read();
+                while (ch != -1) {
+                    if (ch == '\n') {
+                        num++;
+                    }
+
+                    if(line == (num+1)) {
+                        // Inicio rLine
+                        //m_linea = rLine(is_rl, ch, line);
+                        StringBuffer sb = new StringBuffer();
+                        int xch;
+                        try {
+                            //ch = is.read();
+                            while ((xch=is_rl.read()) != '\n') {
+                                if(line==1) {
+                                    sb.append((char)ch);
+                                    line++;
+                                }
+                                if(xch == -1)
+                                    break;
+                                sb.append((char)xch);
+
+                                //ch = is.read();
+                            }
+
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                        m_linea = sb.toString();
+                        // Fin rLine
+                        break;
+                    }
+                    ch = is_rl.read();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            
+            try {
+                ptr_file.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            is_rl = null;
+            // readline fin
+
+
             String data[] = txt.split(m_linea);
             setSuministro(data[0]);
             m_current = cs;
             
-
             // Setea el suministro actual
             currentSuministro = data[0];
 
@@ -166,7 +228,6 @@ public class canvasForm extends Canvas {
         if(i == 1000) {
         } else {
             while(m_rms.tieneData(m_current) && i < 1000) {
-                ;
                 m_current = m_current + 1;
                 
             }
