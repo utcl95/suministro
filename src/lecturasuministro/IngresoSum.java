bbb22cc15aec466704e9cff579e5f45ec4022325
Juan Aquino <utcl95@gmail.com>
2010-01-24 06:01:34 +0000

    Descomentar linea q realizaba la grabacion de datos...

--- src/lecturasuministro/IngresoSum.java
+++ src/lecturasuministro/IngresoSum.java
@@ -77,9 +77,10 @@ public class IngresoSum extends MIDlet implements CommandListener, ItemCommandLi
         status = c.getCommandType() == Command.OK;
 
             if (c.getCommandType() == Command.OK) {
+                lectura.datosConsumo();
                 canvas.doNext();
                 display2.setCurrent(canvas);
-                //lectura.datosConsumo();
+                
 
             } else if (c.getCommandType() == Command.BACK) {                
                         display2.setCurrent(mainForm);
