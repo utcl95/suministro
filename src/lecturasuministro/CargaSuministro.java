03450dbb4afcf354d28792f79b580d55124456a6
Juan Aquino <utcl95@gmail.com>
2010-01-29 18:06:58 +0000

    Removido el uso de ConfigData, por ahora...

--- src/lecturasuministro/CargaSuministro.java
+++ src/lecturasuministro/CargaSuministro.java
@@ -28,13 +28,6 @@ public class CargaSuministro extends MIDlet {
 
         String msg = "Suministros Cargados : " + m_nsuministros;
 
-        // Grabar el numero de suministros en el RMS.
-        ConfigData cd = new ConfigData();
-        cd.setConfigData(m_nsuministros);
-        // Solo para prueba.
-        //cd.showRMS();
-        cd = null;
-
         Alert al = new Alert(msg);
         al.setTimeout(Alert.FOREVER);
         display.setCurrent(al); 
