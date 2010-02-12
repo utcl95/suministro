d6f579e314d9cd9db228514ccb893e7815fcf4ad
Jaqueline Aquino <das5797@gmail.com>
2010-02-12 04:34:02 +0000

    pantalla para busqeda

---
+++ src/lecturasuministro/BusqedaSuministro.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lecturasuministro;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.MIDlet;
+
+
+/**
+ * CustomItem demo
+ */
+public class BusqedaSuministro extends MIDlet implements CommandListener {
+    private static final Command CMD_EXIT = new Command ("Exit", Command.EXIT, 1);
+    private Display display;
+    private boolean firstTime;
+    private Form mainForm;
+
+    public BusqedaSuministro () {
+        firstTime = true;
+        mainForm = new Form ("Custom Item");
+    }
+
+    protected void startApp () {
+        if (firstTime) {
+            display = Display.getDisplay (this);
+            mainForm.append (new FormCanvas ("Suministro", Display.getDisplay (this)));
+            mainForm.append (new TextField ("Consumo", null, 10, 0));
+            mainForm.append (new TextField ("Observación", null, 10, 0));
+            
+            mainForm.addCommand (CMD_EXIT);
+            mainForm.setCommandListener (this);
+            firstTime = false;
+        }
+
+        display.setCurrent (mainForm);
+    }
+
+    public void commandAction (Command c, Displayable d) {
+        if (c == CMD_EXIT) {
+            destroyApp (false);
+            notifyDestroyed ();
+        }
+    }
+
+    protected void destroyApp (boolean unconditional) {
+    }
+
+    protected void pauseApp () {
+    }
+}
