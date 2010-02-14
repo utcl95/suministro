415a0c7c2dd700b57f4c3983a568fe411a2dbd0f
Jaqueline Aquino <das5797@gmail.com>
2010-02-14 21:39:14 +0000

    completo

--- src/lecturasuministro/BusqedaSuministro.java
+++ src/lecturasuministro/BusqedaSuministro.java
@@ -18,7 +18,18 @@ public class BusqedaSuministro extends MIDlet implements CommandListener, ItemCo
     private Display display;
     private boolean firstTime;
     private Form mainForm;
-
+    private Alert yesNoAlert;
+    private Command softKey1;
+    private Command softKey2;
+    private boolean status;
+    private TextField consumo;
+    private TextField obs;
+    private int vobs;
+    private int lactual;
+    private String suministro;
+    RMS_Suministro sRMS = new RMS_Suministro("SUMINISTROS");
+    private FormCanvas objCanvas;
+    
     public BusqedaSuministro () {
         firstTime = true;
         mainForm = new Form ("Custom Item");
@@ -27,10 +38,12 @@ public class BusqedaSuministro extends MIDlet implements CommandListener, ItemCo
     protected void startApp () {
         if (firstTime) {
             display = Display.getDisplay (this);
-            mainForm.append (new FormCanvas ("Suministro", Display.getDisplay (this)));
-            mainForm.append (new TextField ("Consumo   ", "", 12, TextField.NUMERIC));
-            mainForm.append (new TextField ("Obs", "", 2, TextField.NUMERIC));
-
+            objCanvas = new FormCanvas ("Suministro", Display.getDisplay (this));
+            mainForm.append (objCanvas);
+            consumo = new TextField("Consumo   ", "", 12, TextField.NUMERIC);
+            obs = new TextField ("Obs", "", 2, TextField.NUMERIC);
+            mainForm.append(consumo);
+            mainForm.append (obs);
             StringItem item = new StringItem("", "Ingresar", Item.BUTTON);
             item.setDefaultCommand(CMD_PRESS2);
             item.setItemCommandListener(this);
@@ -38,15 +51,26 @@ public class BusqedaSuministro extends MIDlet implements CommandListener, ItemCo
             mainForm.addCommand (CMD_EXIT);
             mainForm.setCommandListener (this);
             firstTime = false;
+            
         }
-
         display.setCurrent (mainForm);
+        
     }
 
     public void commandAction (Command c, Displayable d) {
         if (c == CMD_EXIT) {
             destroyApp (false);
             notifyDestroyed ();
+        }
+
+        if (c.getCommandType() == Command.OK) {
+            grabarConsumo();
+            consumo.setString("");
+            obs.setString("");
+            display.setCurrent(mainForm);
+
+        }else if (c.getCommandType() == Command.STOP){
+            display.setCurrent(mainForm);
         }
     }
 
@@ -57,5 +81,83 @@ public class BusqedaSuministro extends MIDlet implements CommandListener, ItemCo
     }
 
     public void commandAction(Command c, Item item) {
+    Validacion validarSuministro = new Validacion();
+    
+    suministro = objCanvas.getSuministro();
+    
+        int lanterior = 0;
+        int promedio = 0;
+
+        if(obs.getString().equals("")){
+            vobs = 0;
+        }else{
+            vobs = Integer.parseInt(obs.getString());
+            lactual = Integer.parseInt(consumo.getString());
+        }
+
+        if (vobs  >= 0 && vobs <= 20){
+
+        }else {
+           mostrarAlerta();
+        }
+      
+        lanterior = objCanvas.getAnterior();
+        promedio = objCanvas.getPromedio();
+        int cons_act = lactual - lanterior;
+
+        if (c == CMD_PRESS2){
+            if(!validarSuministro.esValido(vobs, lactual, lanterior, cons_act, promedio) ) {
+                mostrarMensaje("c", lactual);
+            }else{
+                grabarConsumo();
+            }
+        }       // end if
+
+        validarSuministro = null;
+    }
+
+     public void grabarConsumo(){
+        int index = 0;
+      
+            index = getIdSuministro();
+      
+        sRMS.setSuministro(index, suministro, consumo.getString(), obs.getString());
+        //repaintCanvasAfterSave();
+      
+    }
+
+     public void mostrarAlerta(){
+        Alert al1 = new Alert("Atención");
+        al1.setString("Observación Incorrecta.");
+        display.setCurrent(al1);
+     }
+
+     public void mostrarMensaje(String m, int lectura_actual){
+        String mns = m;
+
+        if(mns.equals("d")){
+            String msg = "Lectura correcta";
+            Alert al = new Alert(msg);
+            System.out.println("jaqui4");
+            display.setCurrent(al);
+            System.out.println("jaqui5");
+        }else{
+            yesNoAlert = new Alert("Atencion");
+            yesNoAlert.setString("Consumo incorrecto. Desea guardar consumo : " + lectura_actual);
+            softKey1 = new Command("No", Command.STOP, 1);
+            softKey2 = new Command("Yes", Command.OK, 1);
+            yesNoAlert.addCommand(softKey1);
+            yesNoAlert.addCommand(softKey2);
+            yesNoAlert.setCommandListener(this);
+            System.out.println("jaqui6");
+            display.setCurrent(yesNoAlert);
+            System.out.println("jaqui7");
+            status = false;
+        }
+    }
+
+     public int getIdSuministro() {
+        int i = objCanvas.getCurrentSuministroPosition();
+        return i;
     }
 }
