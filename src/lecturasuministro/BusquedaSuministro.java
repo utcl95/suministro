fce2d45fdc7e592c440f16f924778a5fe822c795
Juan Aquino <utcl95@gmail.com>
2010-03-02 18:58:01 +0000

    Removido el uso de System.out.println

--- src/lecturasuministro/BusquedaSuministro.java
+++ src/lecturasuministro/BusquedaSuministro.java
@@ -100,7 +100,6 @@ public class BusquedaSuministro extends MIDlet implements CommandListener, ItemC
             Validacion validarSuministro = new Validacion();
 
             if ((c.getCommandType() == Command.OK) && (c != CMD_SAVE)) {
-                System.out.println("Entro 9");
                 grabarConsumo();
                 consumo.setString("");
                 obs.setString("");
@@ -137,29 +136,24 @@ public class BusquedaSuministro extends MIDlet implements CommandListener, ItemC
                 boolean consumoEsValido  = validarSuministro.esValido(vobs, lactual, lanterior, cons_act, promedio);
                 boolean obsEsValido = (vobs > 0 && vobs <= 40);
                 boolean obsEsCero = (vobs == 0);
-                System.out.println("Entro 1");
                 if((consumoEsValido && (obsEsValido || obsEsCero)) || (obsEsValido && (lactual == 0) )){
                     grabarConsumo();
-                    System.out.println("Entro 2");
                     consumo.setString("");
                     obs.setString("");
                     return;
                 }
 
                 if((!consumoEsValido && obsEsValido) || (!consumoEsValido && obsEsCero && (lactual != 0))){
-                    System.out.println("Entro 3");
                      mostrarMensaje(3, lactual);
                      return;
                 }
 
                 if((lactual==0 && !obsEsValido) || (!consumoEsValido && !obsEsValido) || (lactual == 0 && obsEsCero) ){
-                    System.out.println("Entro 4");
                      mostrarMensaje(2, lactual);
                      return;
                 }
 
                 if(consumoEsValido && !obsEsValido && !obsEsCero){
-                    System.out.println("Entro 5");
                     mostrarMensaje(4, lactual);
                     return;
                 }
